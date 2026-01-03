8fb9252bf909e8948ef49040b102e2f61b31c700
@#S1u2j3 <sujan7neupane@gmail.com>
2026-01-03 11:37:09 +0545

    add cart controller functionality

---
+++ backend/src/models/cart.models.js
@@ -0,0 +1,37 @@
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      // user model refrence comes here from User model
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    cartData: [
+      {
+        dish: {
+          // dish refrence comes from dish model
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Dish",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Cart = mongoose.model("Cart", cartSchema);
